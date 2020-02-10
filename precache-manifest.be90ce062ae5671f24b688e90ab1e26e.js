a04690232cc1778f8a4c8bf0d81751def1fd1e82
Ksyusha <ksenia.smartsev@gmail.com>
2020-02-10 12:41:03 +0200

    Updates

---
+++ precache-manifest.be90ce062ae5671f24b688e90ab1e26e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9a56b91575e295477d376fa75350a5c",
+    "url": "/books/index.html"
+  },
+  {
+    "revision": "c5cd14f1559bf1b034de",
+    "url": "/books/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "6bc2dff26fdc4a2f4f9e",
+    "url": "/books/static/css/main.fc9dfa0d.chunk.css"
+  },
+  {
+    "revision": "c5cd14f1559bf1b034de",
+    "url": "/books/static/js/2.3b352ac4.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/books/static/js/2.3b352ac4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bc2dff26fdc4a2f4f9e",
+    "url": "/books/static/js/main.951888ac.chunk.js"
+  },
+  {
+    "revision": "e19daf525f26e58dda2d",
+    "url": "/books/static/js/runtime-main.cf21fbf7.js"
+  },
+  {
+    "revision": "46b98f37727541e98e42ef003e5cdf9f",
+    "url": "/books/static/media/books.46b98f37.jpg"
+  }
+]);
